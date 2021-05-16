182743420678050f415404ea0e674ceea937d5c0
bessam-haj-salem <bessamhajsalem@gmail.com>
2021-05-16 11:44:16 +0100

    starter code and building tokenFarm smart contract$

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,24 @@
+const DappToken = artifacts.require('DappToken');
+const DaiToken = artifacts.require('DaiToken');
+const TokenFarm = artifacts.require("TokenFarm");
+
+module.exports = async function(deployer, network,accounts) {
+  //deploy Mock DAI token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed()
+
+   //deploy Mock DAPP token
+   await deployer.deploy(DappToken);
+   const dappToken = await DappToken.deployed()
+
+  // Deploy TokenFarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+
+  //transfers all tokens to TokenFarm (1 million)
+  await dappToken.transfer(token.address,'1000000000000000000000000')
+
+  //transfers 100 Mock DAI tokens to investor
+  await daiToken.transfer(accounts[1],'100000000000000000000')
+};
+
